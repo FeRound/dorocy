15e4fbb3b80bddc09fb56951e7a97f160d8df51a
ze2213 <kch971203@gmail.com>
2022-12-04 19:45:23 +0900

    galley.js 수정
    1.주석제거
    2.페이지function 버그 수정

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,6 +1,5 @@
 $(function(){
-  // srcipt영역 gallery script 만들기 예
-  $("#navi a").click(function(){
+  $("#navi a").click(function(){ // srcipt영역 gallery script 만들기 예
     let imgSrc = $(this).attr("href");
 
     let checkAni = $("#main img:last").is(":animated");
@@ -15,37 +14,28 @@ $(function(){
         }})                                      
     }
     return false
-    // 웹 브라우가 우선으로 작동 되기떄문에 스크립트로 실행시킬려면 넣어야함
   })
 
-  // const pwMargin = ($("#navi .page-wrap").css("margin-left"));
-  // console.log(parseInt(pwMargin));
-  // parseInt : string 또는 다른 자료형을 정수형으로 변환해주는 함수
-
-  // Prev, next 버튼으로 작동하는 스크립트
   const marginNumber = 600;
   let pageLeng = $("#navi .page-wrap .page").length; //페이지 개수
-  let pwElem = $("#navi .page-wrap")
   let pageElem = $("#navi .page-wrap .page")
-
+  let pwElem = $("#navi .page-wrap")
   pwElem.css("width",parseInt(pageElem.css("width")) * pageLeng)
+
   function pageBtnFunc(el){
     el.click(function(){
-
       let marginLeftPw = parseInt(pwElem.css("margin-left"))
       let isAni = pwElem.is(":animated"); //애니메이션 진행여부
-      
-          if($(el).hasClass("next") && marginLeftPw > -(marginNumber * 3)){
+          if($(el).hasClass("next") && marginLeftPw > -(marginNumber * 2)){
             pwElem.animate({marginLeft: `${marginLeftPw - marginNumber}`},"fast")
           }else if($(el).hasClass("prev") && marginLeftPw < 0){
             pwElem.animate({marginLeft: `${marginLeftPw + marginNumber}`},"fast")
-          }else if(marginLeftPw == -(marginNumber * 3) || marginLeftPw == 0){
+          }else if(marginLeftPw == -(marginNumber * 2) || marginLeftPw == 0){
             alert("더 이상 이미지가 없습니다.");
           }
-      console.log(marginLeftPw)
     })
   }
-    
+
   $("img.btn").each(function(){
     pageBtnFunc($(this))
     console.log($(this))
